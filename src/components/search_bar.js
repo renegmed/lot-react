5178ea4ea01ebe2e2a807d55f248a45759560ac2
rene <renegmed@yahoo.com>
2018-07-14 22:02:40 -0400

    commit working app

---
+++ src/components/search_bar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+// const SearchBar = () => {
+//     return <input />;  // React .createElement()
+// };
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+    }
+
+    render() {
+        return (
+            <div className="search-bar col-md-8"> 
+                <input 
+                value={this.state.term}
+                onChange={event => this.onInputChange(event.target.value)} />
+               
+            </div>
+        );
+    }
+
+    onInputChange(term) {
+        this.setState({term});
+        this.props.onSearchTermChange(term);
+    }
+
+    // onInputChange(event) {
+    //     console.log(event.target.value);
+    // }
+}
+export default SearchBar;
